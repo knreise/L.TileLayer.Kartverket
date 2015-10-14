b314b80b9631cabd0403f004663683277a93609c
Atle Frenvik Sveen <atle.sveen@norkart.no>
2015-10-14 16:48:20 +0200

    add lookup layer name

--- L.TileLayer.Kartverket.js
+++ L.TileLayer.Kartverket.js
@@ -7,10 +7,31 @@
 
         baseUrl: 'http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps',
 
-        layers: ['matrikkel_bakgrunn', 'topo2', 'topo2graatone',
-                 'europa', 'toporaster2', 'sjo_hovedkart2',
-                 'kartdata2', 'norges_grunnkart',
-                 'norges_grunnkart_graatone', 'egk'],
+        layers: [
+            'matrikkel_bakgrunn',
+            'topo2',
+            'topo2graatone',
+            'europa',
+            'toporaster2',
+            'sjo_hovedkart2',
+            'kartdata2',
+            'norges_grunnkart',
+            'norges_grunnkart_graatone',
+            'egk'
+        ],
+
+        layerNames: [
+            'Matrikkel bakgrunn',
+            'Topografisk norgeskart 2',
+            'Topografisk norgeskart 2 gråtone',
+            'Europakart',
+            'Topografisk norgeskart, raster 2',
+            'Sjøkart hovedkartserien 2',
+            null,
+            'Norges Grunnkart',
+            'Norges grunnkart gråtone',
+            'Europeisk grunnkart'
+        ],
 
         initialize: function (layer, options) {
             if (this.layers.indexOf(layer) === -1) {
@@ -34,6 +55,15 @@
 
     L.tileLayer.kartverket.getLayers = function () {
         return L.TileLayer.Kartverket.prototype.layers.slice();
+    };
+
+    L.tileLayer.kartverket.getLayerName = function (layer) {
+        var idx = L.TileLayer.Kartverket.prototype.layers.indexOf(layer);
+        var name = null;
+        if (idx !== -1) {
+            name = L.TileLayer.Kartverket.prototype.layerNames[idx];
+        }
+        return (name !== null) ? name : layer;
     };
 
 }());
